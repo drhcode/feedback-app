980772d4e5fd5b5f152a69a7e469ac3b7a754f9e
drhcode <doriancocka@gmail.com>
2023-01-29 15:51:21 +0100

    added sweet alert to the project.

--- src/App.js
+++ src/App.js
@@ -5,28 +5,46 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import swal from 'sweetalert';
 
 const App = () => {
 
   const [feedback, setFeedback] = useState(FeedbackData)
 
+
   const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete'))
-    {
+    swal({
+      title: 'Are you sure you want to delete this review?',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((deleted)=>{
       setFeedback(feedback.filter((item)=> item.id !== id))
+      if(deleted) {
+        swal('You review has been deleted',{
+          icon: 'success'
+        })
+      }else {
+        swal("Operation canceled".toLocaleUpperCase())
+        setFeedback(feedback)
+      }
     }
+    )
   }
+
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
+    swal('Your feedback has been added')
   }
 
   return (
     <>
     <Header  />
     <div className='container'>
-      <FeedbackStats feedback={feedback} />
+    <FeedbackStats feedback={feedback} />
     <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     <FeedbackForm handleAdd = {addFeedback}/>
     </div>
